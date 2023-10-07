0f6e05b6e019830dfd6509182467fc7f26f2f973
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2023-10-08 00:03:28 +0200

    2nd

---
+++ arch-boki-scripts/133-apps.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+
+
+## APPS ##
+
+sudo pacman -S --needed firefox --noconfirm
+sudo pacman -S --needed firefox-dark-reader --noconfirm
+sudo pacman -S --needed firefox-adblock-plus --noconfirm
+sudo pacman -S --needed firefox-ublock-origin --noconfirm
+sudo pacman -S --needed guvcview --noconfirm
+sudo pacman -S --needed meld --noconfirm
+sudo pacman -S --needed aisleriot --noconfirm
+sudo pacman -S --needed galculator --noconfirm
+sudo pacman -S --needed sublime-text-4 --noconfirm
+sudo pacman -S --needed kate --noconfirm
+sudo pacman -S --needed celluloid --noconfirm
+sudo pacman -S --needed mpv --noconfirm
+sudo pacman -S --needed urxvt-perls --noconfirm
+sudo pacman -S --needed urxvt-resize-font-git --noconfirm
+sudo pacman -S --needed urxvt-fullscreen --noconfirm
+sudo pacman -S --needed alacritty --noconfirm
+sudo pacman -S --needed alacritty-themes --noconfirm
+sudo pacman -S --needed base16-alacritty-git --noconfirm
+sudo pacman -S --needed neovim --noconfirm
+sudo pacman -S --needed gparted --noconfirm
+sudo pacman -S --needed font-manager --noconfirm
+sudo pacman -S --needed thunar --noconfirm
+sudo pacman -S --needed thunar-shares-plugin --noconfirm
+sudo pacman -S --needed thunar-archive-plugin --noconfirm
+sudo pacman -S --needed thunar-volman --noconfirm
+sudo pacman -S --needed kvantum --noconfirm
+sudo pacman -S --needed tilix --noconfirm
+sudo pacman -S --needed github-desktop --noconfirm
+sudo pacman -S --needed gitahead --noconfirm
+sudo pacman -S --needed firedragon --noconfirm
+sudo pacman -S --needed brave-bin --noconfirm
+sudo pacman -S --needed qbittorrent --noconfirm
+sudo pacman -S --needed obsidian --noconfirm
+sudo pacman -S --needed kodi --noconfirm
+sudo pacman -S --needed vlc --noconfirm
+sudo pacman -S --needed flameshot --noconfirm
+sudo pacman -S --needed gimp --noconfirm
+sudo pacman -S --needed gpick --noconfirm
+sudo pacman -S --needed inkscape --noconfirm
+sudo pacman -S --needed krita --noconfirm
+sudo pacman -S --needed okular  --noconfirm
+# sudo pacman -S --needed vscodium-bin --noconfirm
+sudo pacman -S --needed visual-studio-code-bin  --noconfirm
+#sudo pacman -S --needed wps-office --noconfirm
+sudo pacman -S --needed wps-office-mime --noconfirm
+sudo pacman -S --needed ttf-wps-fonts --noconfirm
+sudo pacman -S --needed kruler --noconfirm
+sudo pacman -S --needed remmina --noconfirm
+sudo pacman -S --needed teamviewer --noconfirm
+sudo pacman -S --needed zoom --noconfirm
+sudo pacman -S --needed webapps --noconfirm
+sudo pacman -S --needed handbrake --noconfirm
+sudo pacman -S --needed rhythmbox --noconfirm
+sudo pacman -S --needed xfce4-terminal --noconfirm
+sudo pacman -S --needed xfce4-terminal-base16-colors-git --noconfirm
+sudo pacman -S --needed tempus-themes-xfce4-terminal-git --noconfirm
+sudo pacman -S --needed vim --noconfirm
+sudo pacman -S --needed neovim-qt --noconfirm
+sudo pacman -S --needed rofi --noconfirm
+sudo pacman -S --needed variety --noconfirm
+sudo pacman -S --needed arch-boki-variety-git --noconfirm
+sudo pacman -S --needed arch-boki-variety-autostart-git --noconfirm
