b53a57481fe75d86feed23168cd0e0b8124b2424
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2023-10-14 08:29:14 +0200

    add obs-studio install script

---
+++ arch-boki-scripts/137-obs-studio.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# OBS VkCapture Plugin              com.obsproject.Studio.Plugin.OBSVkCapture   
+# GStreamer Plugin                  com.obsproject.Studio.Plugin.Gstreamer        
+# GStreamer VA-API Plugin           com.obsproject.Studio.Plugin.GStreamerVaapi 
+# Transition Table Plugin           com.obsproject.Studio.Plugin.TransitionTable 
+# Waveform                          com.obsproject.Studio.Plugin.waveform 
+# Input Overlay Plugin              com.obsproject.Studio.Plugin.InputOverlay 
+# Advanced Scene Switcher Plugin    com.obsproject.Studio.Plugin.SceneSwitcher 
+# Move Transition Plugin            com.obsproject.Studio.Plugin.MoveTransition 
+# Scale To Sound Plugin             com.obsproject.Studio.Plugin.ScaleToSound 
+# WebSocket Server (Legacy)         com.obsproject.Studio.Plugin.WebSocket 
+# Background Removal                com.obsproject.Studio.Plugin.BackgroundRemoval
+# LiveSplit One                     com.obsproject.Studio.Plugin.OBSLivesplitOne  
+# DroidCam                          com.obsproject.Studio.Plugin.DroidCam    
+# Vertical Canvas                   com.obsproject.Studio.Plugin.VerticalCanvas  
+# OBS-Studio                        com.obsproject.Studio
+
+flatpak install flathub com.obsproject.Studio
+flatpak install com.obsproject.Studio.Plugin.OBSVkCapture   
+flatpak install com.obsproject.Studio.Plugin.Gstreamer        
+flatpak install com.obsproject.Studio.Plugin.GStreamerVaapi 
+flatpak install com.obsproject.Studio.Plugin.TransitionTable 
+flatpak install com.obsproject.Studio.Plugin.waveform 
+flatpak install com.obsproject.Studio.Plugin.InputOverlay 
+flatpak install com.obsproject.Studio.Plugin.SceneSwitcher 
+flatpak install com.obsproject.Studio.Plugin.MoveTransition 
+flatpak install com.obsproject.Studio.Plugin.ScaleToSound 
+flatpak install com.obsproject.Studio.Plugin.WebSocket 
+flatpak install com.obsproject.Studio.Plugin.BackgroundRemoval
+flatpak install com.obsproject.Studio.Plugin.OBSLivesplitOne  
+flatpak install com.obsproject.Studio.Plugin.DroidCam    
+flatpak install com.obsproject.Studio.Plugin.VerticalCanvas 
+
+## another way 
+## flatpak install flathub com.obsproject.Studio com.obsproject.Studio.Plugin.OBSVkCapture com.obsproject.Studio.Plugin.Gstreamer com.obsproject.Studio.Plugin.GStreamerVaapi com.obsproject.Studio.Plugin.TransitionTable com.obsproject.Studio.Plugin.waveform com.obsproject.Studio.Plugin.InputOverlay com.obsproject.Studio.Plugin.SceneSwitcher com.obsproject.Studio.Plugin.MoveTransition com.obsproject.Studio.Plugin.ScaleToSound com.obsproject.Studio.Plugin.WebSocket com.obsproject.Studio.Plugin.BackgroundRemoval com.obsproject.Studio.Plugin.OBSLivesplitOne com.obsproject.Studio.Plugin.DroidCam com.obsproject.Studio.Plugin.VerticalCanvas 
